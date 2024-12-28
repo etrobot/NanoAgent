21654993a3762afdd8d643225186fd65afa14dd6
franklin <etrobot@outlook.com>
2024-12-28 15:44:48 +0800

    refactor DebugLogger

--- NanoAgent/core.py
+++ NanoAgent/core.py
@@ -1,31 +1,7 @@
 import json
 import openai
 import tiktoken
-
-class DebugLogger:
-    def __init__(self, debug=False):
-        self.debug = debug
-        self.colors = {
-            'user': '\033[93m',
-            'assistant': '\033[94m',
-            'action': '\033[95m',
-            'reason': '\033[96m',
-            'next_prompt': '\033[92m',
-            'reset': '\033[0m'
-        }
-
-    def log(self, category, message, end='\n', flush=True):
-        if not self.debug:
-            return
-        if category in self.colors:
-            color = self.colors[category]
-            label = category.title() + ':'
-            print(f"{color}{label}\033[0m {message}", end=end, flush=flush)
-
-    def print(self, message, end='', flush=True):
-        if not self.debug:
-            return
-        print(message, end=end, flush=flush)
+from .logger import DebugLogger
 
 class NanoAgent:
     def __init__(self,api_key:str,base_url:str,model:str,max_tokens:int,actions=[],debug=False):                
---
+++ NanoAgent/logger.py
@@ -0,0 +1,24 @@
+class DebugLogger:
+    def __init__(self, debug=False):
+        self.debug = debug
+        self.colors = {
+            'user': '\033[93m',
+            'assistant': '\033[94m',
+            'action': '\033[95m',
+            'reason': '\033[96m',
+            'next_prompt': '\033[92m',
+            'reset': '\033[0m'
+        }
+
+    def log(self, category, message, end='\n', flush=True):
+        if not self.debug:
+            return
+        if category in self.colors:
+            color = self.colors[category]
+            label = category.title() + ':'
+            print(f"{color}{label}\033[0m {message}", end=end, flush=flush)
+
+    def print(self, message, end='', flush=True):
+        if not self.debug:
+            return
+        print(message, end=end, flush=flush) 
